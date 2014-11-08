f0be48bec270f1e75a87951dc3fa14dae1ac3dd9
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2014-11-08 18:39:42 -0500

    Tweaking image filter css prop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   var image = new Image();
   image.src = "assets/img/bd3.jpg";
+  //after image is loaded, display page
   image.onload = function()
   {
     $('body').animate({opacity:'toggle'},750);
