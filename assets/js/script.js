942d0af6e9471a9beaf099406ea04a94862f6428
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2014-12-26 07:31:47 +0000

    Adding animations to the home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
   $(".menu-btn").click(function(){
     $(".menu").addClass("active");
   });
+  
+  $(".close-btn").click(function(){
+    $(".menu").removeClass("active");
+  });
+  
 });
