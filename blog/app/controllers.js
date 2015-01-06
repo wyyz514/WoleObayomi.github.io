5b017d362aec252e430e682469fd94696624026d
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-01-06 14:59:58 -0500

    Added controllers

--- blog/app/controllers.js
+++ blog/app/controllers.js
@@ -1,5 +1,21 @@
 blog.controller("IndexController",["$scope","FirebaseService",function($scope,FirebaseService){
+  FirebaseService.establishConnection();
+}]);
+
+blog.controller("NewPostController",["$scope","FirebaseService",function($scope,FirebaseService){
+  $scope.post = {};
+  $scope.post.title = "";
+  $scope.post.body = "";
   
+  var newPost = {};
+  newPost.id = FirebaseService.generateID();
+  
+  $scope.savePost = function()
+  {
+    newPost.title = $scope.post.title;
+    newPost.body = $scope.post.body;
+    FirebaseService.addPost(newPost);
+  }
 }]);
 
 blog.controller("EditController",["$scope","FirebaseService",function($scope,FirebaseService){}]);
