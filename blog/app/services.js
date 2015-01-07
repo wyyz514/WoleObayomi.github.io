f7a8cd273d5ee114c17b99df42ca5fdf9fe1bb65
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-01-07 16:48:31 -0500

    removed unneeded functions

--- blog/app/services.js
+++ blog/app/services.js
@@ -1,38 +1,38 @@
 blog.service("FirebaseService",["$firebase",function($firebase){
   var link = "https://shining-torch-6695.firebaseio.com/blog/posts";
+  var posts = [];
   var firebase = {
-    posts:[],
     selectedPostKey:"",
     establishConnection:function()
     {
       var ref = new Firebase(link);
       var sync = $firebase(ref);
       var syncArray = sync.$asArray();
-      this.posts = syncArray;
+      posts = syncArray;
     },
     getPosts:function()
     {
-      return this.posts;
+      return posts;
     },
     getPost:function(key)
     {
-      return this.posts.$getRecord(key);
+      return posts.$getRecord(key);
     },
     addPost:function(post)
     {
-      this.posts.$add(post);
+      posts.$add(post);
     },
     deletePost:function(post)
     {
-      this.posts.$remove(post);
+      posts.$remove(post);
     },
     updatePost:function(post)
     {
-      this.posts.$save(post);
+      posts.$save(post);
     },
     generateID:function()
     {
-      return this.posts.length + 1;
+      return posts.length + 1;
     }
   };
   return firebase;
