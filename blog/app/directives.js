9cd6d30c86b559de639ac51f3d0f6017e80e87c7
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2015-01-08 09:55:06 -0500

    Edited directive template. Need to find a better way to implement lines

--- blog/app/directives.js
+++ blog/app/directives.js
@@ -1,13 +1,16 @@
 blog.directive("cards",function(){
   return {
     restrict:"A",
-    template:"<div class='card'><div class='card-inner'><span>{{post.title}}</span></div></div>",
+    template:"<div class='card'><div class='card-inner'><span>{{post.title}}</span><br><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div><div class='line'></div></div></div>",
     link:function(scope,el,attrs)
     {
       var card = el[0].children[0];
       var randMult = Math.random();
       var degrRot = randMult * 20;
       card.style.webkitTransform = "rotate("+degrRot+"deg)";
+      card.style.mozTransform = "rotate("+degrRot+"deg)";
+      card.style.msTransform = "rotate("+degrRot+"deg)";
+      card.style.transform = "rotate("+degrRot+"deg)";
     }
   };
 });
